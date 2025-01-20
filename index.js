d6af93b4489a766de93f55af1e247d1fb05a091f
sikwan <yysskk99999@naver.com>
2025-01-20 14:52:21 +0900

    server text

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    res.setHeader("Content-type", "text/plain");
+    res.write("Hello node");
+    res.end();
 });
 
 
